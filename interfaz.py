d7a1c6f0bd8683e8639505d400040985bd099641
Aruroo <jonathan.arturo.preciado@ciencias.unam.mx>
2022-08-26 23:30:33 -0500

    interfaz++

--- interfaz.py
+++ interfaz.py
@@ -1,14 +1,18 @@
 from tkinter import*
 
-raiz= Tk()
-raiz.title("Clima")
-raiz.resizable(False,False)
-raiz.geometry("700x500")
-raiz.config(bg="gray")
-cuadro=Frame()
-cuadro.pack()
-cuadro.config(bg="blue")
-cuadro.config(width=650, height= 500)
-cuadro.config(bd=20)
-cuadro.config(relief="groove")
-raiz.mainloop()
+class Interfaz():
+    
+    def ejecutaInterfaz(): 
+        raiz= Tk()
+        raiz.title("Clima")
+        raiz.resizable(False,False)
+        raiz.geometry("700x500")
+        raiz.config(bg="gray")
+        cuadro=Frame()
+        cuadro.pack()
+        cuadro.config(bg="blue")
+        cuadro.config(width=650, height= 500)
+        cuadro.config(bd=20)
+        cuadro.config(relief="groove")
+        raiz.mainloop()
+
