4c0b71674af8b0f5dff362f4276657826e94768e
Aruroo <jonathan.arturo.preciado@ciencias.unam.mx>
2022-08-28 17:35:17 -0500

    pruebas de interfaz

---
+++ generaClima.py
@@ -0,0 +1,8 @@
+    #archivo para generar pruebas con la API
+    #key= ''
+    #url = 'https://api.openweathermap.org/data/2.5/weather?id=524901&lang=es&appid='+'{'+key+'}'
+    #respuesta = requests.get(url)
+    #contenido = respuesta.content
+    #archivo =open('clima.json','wb')
+    #archivo.write(contenido)
+    #archivo.close
--- interfaz.py
+++ interfaz.py
@@ -1,18 +1,23 @@
 from tkinter import*
+import requests
 
 class Interfaz():
     
     def ejecutaInterfaz(): 
         raiz= Tk()
+        cuadro=Frame(width=650, height= 500,bd=20,relief="groove",bg="gray")
+        #bg es el grosor del borde del frame
+        ciudadOrigenLabel=Label(cuadro, text='Inserte su ciudad de origen:', justify="center",bg="blue", fg='red', font=("Arial", 20))
+        imagenNube= PhotoImage(file="nube.png")
+        imagenNube=imagenNube.subsample(10)
+        Label(cuadro, image=imagenNube).place(x=500, y=5)
         raiz.title("Clima")
         raiz.resizable(False,False)
         raiz.geometry("700x500")
         raiz.config(bg="gray")
-        cuadro=Frame()
+        
         cuadro.pack()
-        cuadro.config(bg="blue")
-        cuadro.config(width=650, height= 500)
-        cuadro.config(bd=20)
-        cuadro.config(relief="groove")
+        ciudadOrigenLabel.place(x=20, y=5) #permite colocar el label en una coordenada dada
         raiz.mainloop()
 
+    ejecutaInterfaz()
