b6bc806a449b4e098defe7df95e05a5b9d27d177
Jonathan A. Preciado R <72569761+Aruroo@users.noreply.github.com>
2022-08-31 18:18:13 -0500

    Delete generaClima.py
    incluye mi llave

--- generaClima.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-key= "feb7f94192f44091b4100d55e418590d"
-url = 'https://api.openweathermap.org/data/2.5/weather?id=524901&lang=es&appid='+key
-respuesta = requests.get(url)
-contenido = respuesta.content
-archivo =open('clima.json','wb')
-archivo.write(contenido)
-archivo.close
