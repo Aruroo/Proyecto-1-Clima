e3dad839ec899ad913b30bfce97da27880b0d433
Jorge Velasco García <ge0rg1vg@MBPJorge.local>
2022-09-11 14:10:18 -0500

    Diccionario agregado

--- src/clima/Clima.py
+++ src/clima/Clima.py
@@ -1,42 +1,65 @@
 class Clima ():
 
-    """
-        Función que se encarga de iniciar el proceso de consulta de clima con
-        base en el archivo .csv que es pasado como parámetro.
-    """
-    def function1():
-        c1 = Ciudad()
-        c2 = Ciudad()
+    def __init__(self):
+        diccionario = DiccionarioCiudades()
 
-    """
-        Función que se encarga de procesar la petición o bien, arrojar datos de
-        la ciudad dada por el usuario en la función 1.
-    """
-    def function2():
+    def método1():
         """
-        revisar en caché
+            Método que se encarga de procesar la petición o bien, arrojar datos de
+            la ciudad dada por el usuario en la interfaz
         """
 
-        """
-        Bandera para saber si la información solicitada se encuentra en caché.
-        True si se encuentra la información en caché.
-        False si no se encuentra la información en caché.
-        """
-        b = true
+        """Ciudad de destino dado por el usuario (nombre): ciudaddestino"""
 
-        if b:
-            """
-            Devuelve el archivo .json que contiene los datos a recolectar necesarios.
-            """
+
+        """Busca en el diccionario las coordenadas"""
+        coordenadasciudad = funciónAux(ciudaddestino)
+
+        """revisar en caché"""
+
+
+
+        """Bandera dada por la función auxiliar 2 para saber si la información
+        solicitada se encuentra en caché. True si se encuentra la información en caché.
+        False si no se encuentra la información en caché."""
+        bandera1 = funciónAux2(ciudaddestino)
+
+        if bandera1:
+
+            """Devuelve el archivo .json que contiene los datos a recolectar necesarios."""
+
             pass
         else:
-            """
-            Realizar petición para devolver un archivo .json.
-            """
 
-    """
-        Función que se encarga de recolectar los datos necesarios del archivo
-        .json dado por la función anterior.
-    """
-    def function3():
-        return
+            """Realizar petición para devolver un archivo .json."""
+
+    def funciónAux(ciudad):
+        """
+            Función auxiliar que se encarga de realizar la búsqueda de coordenadas
+            de las ciudades pasadas como parámetro, en el diccionario
+        """
+        for i in range(1, diccionario.tamaño):
+            if ciudad == diccionario.ciudades["nombres"][i]:
+                print("Mostrando los datos de la ciudad", i)
+                print("Nombre:", diccionario.ciudades["nombres"][i])
+                print("Coordenadas:", diccionario.ciudades["coordenadas"][i])
+                return diccionario.ciudades["coordenadas"][i]
+
+    def funciónAux2(ciudad):
+        """
+            Función auxiliar que se encarga de comprobar si la información de la
+            ciudad dada comp parámetro se encuentra en caché. Devuelve True si se
+            encuentra en caché o False si no está en caché.
+        """
+        for i in range(1, diccionario.tamaño):
+            if ciudad == diccionario.ciudades["nombres"][i]:
+                print("Mostrando los datos de la ciudad", i)
+                print("Nombre:", diccionario.ciudades["nombres"][i])
+                print("Coordenadas:", diccionario.ciudades["coordenadas"][i])
+                return diccionario.ciudades["coordenadas"][i]
+
+    def método2():
+        """
+            Método que se encarga de recolectar los datos necesarios del archivo
+            .json dado por el método anterior.
+        """
