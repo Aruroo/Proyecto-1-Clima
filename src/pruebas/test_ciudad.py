45299bd17367121da4e732354385e04815224c35
Jorge Velasco García <ge0rg1vg@MBPJorge.local>
2022-09-16 21:56:09 -0500

    Pruebas y pdf agregados

--- src/pruebas/test_ciudad.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestCiudad(unittest.TestCase):
-
-    def test_constructor_ciudad(self):
-        """
-        Prueba que el constructor de la ciudad sea correcto
-        """
--- src/pruebas/test_clima.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestClima(unittest.TestCase):
-
-    def test_constructor_clima(self):
-        """
-        Prueba que el constructor del clima sea correcto
-        """
